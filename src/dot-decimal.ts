524dc2323f7096994495c2d1d973846d0b2bb56e
Daniel Kraemer <dkraemer@dkross.org>
2020-07-03 21:06:55 +0000

    Redesign DotDecimal

--- src/dot-decimal.ts
+++ src/dot-decimal.ts
@@ -1,43 +1,92 @@
 import { Dumpable } from './dumpable';
 
-const HEX_RADIX: number = 16;
+enum Radix {
+  Dec = 10,
+  Hex = 16,
+}
 
 export abstract class DotDecimal implements Dumpable {
   public readonly hexString: string;
   public readonly dotDecimalString: string;
 
   protected constructor(public readonly value: number) {
-    this.hexString = DotDecimal.toHexString(value);
-    this.dotDecimalString = DotDecimal.toDotDecimalString(value);
-  }
-
-  private static toHexString(value: number): string {
-    return '0x' + (value === 0 ? '00000000' : value.toString(HEX_RADIX));
-  }
-
-  private static toDotDecimalString(value: number): string {
-    let hexString = this.toHexString(value);
-    let regEx = /^(0x)(.{2})(.{2})(.{2})(.{2})$/;
-    let regExMatches = regEx.exec(hexString);
-
-    if (!regExMatches || regExMatches.length !== 6) {
-      console.error('RegEx mismatch!');
-      return '';
+    if (value < 0 || value > 0xffffffff) {
+      throw new Error('Parameter value must be an unsigned integer (32 Bit)');
     }
 
-    let FirstOctet: string = regExMatches[1] + regExMatches[2];
-    let SecondOctet: string = regExMatches[1] + regExMatches[3];
-    let ThirdOctet: string = regExMatches[1] + regExMatches[4];
-    let FourthOctet: string = regExMatches[1] + regExMatches[5];
+    this.hexString = this.toHexString();
+    this.dotDecimalString = this.toDotDecimalString();
+  }
 
-    let decimalStrings: string[] = [
-      Number.parseInt(FirstOctet, HEX_RADIX).toFixed(),
-      Number.parseInt(SecondOctet, HEX_RADIX).toFixed(),
-      Number.parseInt(ThirdOctet, HEX_RADIX).toFixed(),
-      Number.parseInt(FourthOctet, HEX_RADIX).toFixed(),
-    ];
+  private static parseOctets(regExp: RegExp, string: string, radix: Radix): number[] | null {
+    const regRepExecArray = regExp.exec(string);
+    if (!regRepExecArray) {
+      return null;
+    }
 
-    return decimalStrings.join('.');
+    const octetStrings = regRepExecArray.slice(1);
+    if (octetStrings.length !== 4) {
+      return null;
+    }
+
+    const retVal: number[] = [];
+    octetStrings.forEach(e => {
+      const result = Number.parseInt(e, radix);
+      if (!Number.isNaN(result)) {
+        retVal.push(result);
+      }
+    });
+
+    return retVal.length === 4 ? retVal : null;
+  }
+
+  public static parse<T extends DotDecimal>(t: new (value: number) => T, dotDecimalString: string): T | null {
+    const regExp = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/;
+    const octets = this.parseOctets(regExp, dotDecimalString, Radix.Dec);
+    if (!octets) {
+      return null;
+    }
+
+    let hexValue = '0x';
+    octets.forEach(e => {
+      const octetHex = e.toString(Radix.Hex);
+      hexValue += octetHex.length === 1 ? '0' + octetHex : octetHex;
+    });
+
+    const value = Number.parseInt(hexValue);
+    try {
+      return new t(value);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  private toHexString(withPrefix: boolean = true): string {
+    const prefix = withPrefix ? '0x' : '';
+    let hexValue = this.value.toString(Radix.Hex);
+
+    // Ensure a length of 4 bytes (8 chars)
+    while (hexValue.length < 8) {
+      hexValue = '0' + hexValue;
+    }
+
+    return prefix + hexValue;
+  }
+
+  private toDotDecimalString(): string {
+    const regExp = /^(.{2})(.{2})(.{2})(.{2})$/;
+    const hexString = this.toHexString(false);
+    const octets = DotDecimal.parseOctets(regExp, hexString, Radix.Hex);
+    if (!octets) {
+      throw new Error();
+    }
+
+    const octetStrings: string[] = [];
+    octets.forEach(e => {
+      octetStrings.push(e.toFixed());
+    });
+
+    return octetStrings.join('.');
   }
 
   public toString(): string {
@@ -45,12 +94,6 @@ export abstract class DotDecimal implements Dumpable {
   }
 
   public dump(): string[] {
-    let retVal: string[] = [
-      `value: ${this.value}`,
-      `hexString: ${this.hexString}`,
-      `dotDecimalString: ${this.dotDecimalString}`,
-    ];
-
-    return retVal;
+    return [`value: ${this.value}`, `hexString: ${this.hexString}`, `dotDecimalString: ${this.dotDecimalString}`];
   }
 }
---
+++ test/dot-decimal.test.ts
@@ -0,0 +1,32 @@
+import { DotDecimal } from '../src';
+
+class DotDecimalMock extends DotDecimal {
+  constructor(value: number) {
+    super(value);
+  }
+}
+
+test('DotDecimal()', () => {
+  expect(() => {
+    new DotDecimalMock(-1);
+  }).toThrow(Error);
+  expect(() => {
+    new DotDecimalMock(0x1ffffffff);
+  }).toThrow(Error);
+  expect(new DotDecimalMock(1).value).toBe(1);
+  expect(new DotDecimalMock(0).hexString).toBe('0x00000000');
+  expect(new DotDecimalMock(11259375).hexString).toBe('0x00abcdef');
+  expect(new DotDecimalMock(0).dotDecimalString).toBe('0.0.0.0');
+  expect(new DotDecimalMock(2212237532).dotDecimalString).toBe('131.220.16.220');
+  expect(new DotDecimalMock(0x83dc10dc).dotDecimalString).toBe('131.220.16.220');
+  expect(new DotDecimalMock(0b10000011110111000001000011011100).dotDecimalString).toBe('131.220.16.220');
+});
+
+test('DotDecimal.parse()', () => {
+  expect(DotDecimal.parse(DotDecimalMock, '') === null).toBe(true);
+  expect(DotDecimal.parse(DotDecimalMock, 'foo') === null).toBe(true);
+  expect(DotDecimal.parse(DotDecimalMock, '256.0.0.0') === null).toBe(true);
+  expect(DotDecimal.parse(DotDecimalMock, '255.0.0.0') !== null).toBe(true);
+  expect(DotDecimal.parse(DotDecimalMock, '0.0.0.0')?.value).toBe(0);
+  expect(DotDecimal.parse(DotDecimalMock, '131.220.16.220')?.value).toBe(0x83dc10dc);
+});
