dbb22e2d8a5eabaf26338e859dab7b50ae978b90
Daniel Kraemer <dkraemer@dkross.org>
2020-07-04 17:09:34 +0000

    NOTESTS: Finished implementation of SubnetMask

--- src/subnet-mask.ts
+++ src/subnet-mask.ts
@@ -1,46 +1,191 @@
 import { DotDecimal } from './dot-decimal';
-// import { Radix } from './radix';
 
 export class SubnetMask extends DotDecimal {
-  // public readonly prefixLength: number;
-  // public readonly hostBitCount: number;
-  // public readonly hostAddressCount: number;
-  // public readonly usableHostAddressCount: number;
-  // public readonly remark: string;
+  /**
+   * The prefix length of this subnet mask.
+   */
+  public readonly prefixLength: number;
+
+  /**
+   * The number of bits reserved for host addresses.
+   */
+  public readonly hostBitCount: number;
+
+  /**
+   * The number of host addresses.
+   */
+  public readonly hostAddressCount: number;
+
+  /**
+   * The number of usable host addresses.
+   */
+  public readonly usableHostAddressCount: number;
+
+  /**
+   * Remarks for this subnet mask.
+   */
+  public readonly remark: string;
+
+  /**
+   * The maximum prefix length.
+   */
+  public static readonly MAX_PREFIX_LENGTH: number = 32;
+
+  /**
+   * The numerical values of all valid subnet masks.
+   */
+  public static readonly ALL_VALID_VALUES: readonly number[] = [
+    0xffffffff,
+    0xfffffffe,
+    0xfffffffc,
+    0xfffffff8,
+    0xfffffff0,
+    0xffffffe0,
+    0xffffffc0,
+    0xffffff80,
+    0xffffff00,
+    0xfffffe00,
+    0xfffffc00,
+    0xfffff800,
+    0xfffff000,
+    0xffffe000,
+    0xffffc000,
+    0xffff8000,
+    0xffff0000,
+    0xfffe0000,
+    0xfffc0000,
+    0xfff80000,
+    0xfff00000,
+    0xffe00000,
+    0xffc00000,
+    0xff800000,
+    0xff000000,
+    0xfe000000,
+    0xfc000000,
+    0xf8000000,
+    0xf0000000,
+    0xe0000000,
+    0xc0000000,
+    0x80000000,
+    0x00000000,
+  ];
+
+  /**
+   * All valid subnet masks.
+   */
+  public static readonly ALL_VALID: readonly SubnetMask[] = [
+    new SubnetMask(0xffffffff),
+    new SubnetMask(0xfffffffe),
+    new SubnetMask(0xfffffffc),
+    new SubnetMask(0xfffffff8),
+    new SubnetMask(0xfffffff0),
+    new SubnetMask(0xffffffe0),
+    new SubnetMask(0xffffffc0),
+    new SubnetMask(0xffffff80),
+    new SubnetMask(0xffffff00),
+    new SubnetMask(0xfffffe00),
+    new SubnetMask(0xfffffc00),
+    new SubnetMask(0xfffff800),
+    new SubnetMask(0xfffff000),
+    new SubnetMask(0xffffe000),
+    new SubnetMask(0xffffc000),
+    new SubnetMask(0xffff8000),
+    new SubnetMask(0xffff0000),
+    new SubnetMask(0xfffe0000),
+    new SubnetMask(0xfffc0000),
+    new SubnetMask(0xfff80000),
+    new SubnetMask(0xfff00000),
+    new SubnetMask(0xffe00000),
+    new SubnetMask(0xffc00000),
+    new SubnetMask(0xff800000),
+    new SubnetMask(0xff000000),
+    new SubnetMask(0xfe000000),
+    new SubnetMask(0xfc000000),
+    new SubnetMask(0xf8000000),
+    new SubnetMask(0xf0000000),
+    new SubnetMask(0xe0000000),
+    new SubnetMask(0xc0000000),
+    new SubnetMask(0x80000000),
+    new SubnetMask(0x00000000),
+  ];
 
   /**
    * Create a new instance of SubnetMask.
    * @param value The integer value of the subnet mask to create.
    */
   public constructor(value: number) {
+    SubnetMask.validate(value);
     super(value);
+    this.prefixLength = SubnetMask.getPrefixLength(value, false);
+    this.hostBitCount = SubnetMask.MAX_PREFIX_LENGTH - this.prefixLength;
+    this.hostAddressCount = 2 ** this.hostBitCount;
+    this.usableHostAddressCount = this.hostAddressCount - 2;
+
+    switch (this.prefixLength) {
+      case 32:
+        this.usableHostAddressCount = 1;
+        this.remark = '(Host route)';
+        break;
+      case 31:
+        this.usableHostAddressCount = 2;
+        this.remark = '(Point-to-point links (RFC 3021))';
+        break;
+      case 30:
+        this.remark = '(Point-to-point links (glue network))';
+        break;
+      case 8:
+        this.remark = '(Largest IANA block allocation)';
+        break;
+      default:
+        this.remark = '';
+        break;
+    }
   }
 
   /**
-   * Get all possible subnet masks as integer values.
+   * Validate a subnet mask.
+   * @param value The integer value of the subnet mask to validate.
+   * @param shouldThrow When true (default) an error is thrown when the given subnet mask is invalid.
+   * @returns True when the subnet mask is valid or false when it is not.
    */
-  public static getAllValues(): number[] {
-    const allValues: number[] = [];
-    for (let hostBitCount = 0; hostBitCount <= 32; hostBitCount++) {
-      const value = hostBitCount === 32 ? 0 : (0xffffffff << hostBitCount) >>> 0;
-      allValues.push(value);
+  public static validate(value: number, shouldThrow: boolean = true): boolean {
+    if (SubnetMask.ALL_VALID_VALUES.includes(value)) {
+      return true;
     }
-    return allValues;
+    if (shouldThrow) {
+      throw new Error('Invalid subnet mask!');
+    }
+    return false;
   }
 
   /**
    * Get the prefix length for a subnet mask.
    * @param value The integer value of the subnet mask.
+   * @param shouldValidate When true (default) the subnet mask will be validated.
+   * @param shouldThrow When true (default) an error is thrown if the subnet mask is invalid.
+   * @returns The prefix length.
    */
-  public static getPrefixLength(value: number): number {
-    if (!this.getAllValues().includes(value)) {
-      throw new Error();
+  public static getPrefixLength(value: number, shouldValidate: boolean = true, shouldThrow: boolean = true): number {
+    if (shouldValidate) {
+      SubnetMask.validate(value, shouldThrow);
     }
     // Convert value to binary string and count how many bits are set
     return value.toString(2).split('1').length - 1;
   }
-}
 
-// SubnetMask.getAllValues().forEach(e => {
-//   console.log(`${e.toString(16).padStart(8,'0')} => ${SubnetMask.getPrefixLength(e)}`);
-// });
+  /**
+   * Dump all public properties' values in a human-readable format.
+   * @returns An Array<String> containing the dump.
+   */
+  public dump(): string[] {
+    const dump: string[] = super.dump();
+    dump.push(
+      `prefixLength: ${this.prefixLength}`,
+      `hostBitCount: ${this.hostBitCount}`,
+      `hostAddressCount: ${this.hostAddressCount}`,
+      `usableHostAddressCount: ${this.usableHostAddressCount}`,
+      `remark: ${this.remark}`
+    );
+    return dump;
+  }
+}
